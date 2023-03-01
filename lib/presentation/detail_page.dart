e46f3521ab9dbfea03c709327acde311b814ff9e
まさたか <shinonome419@icloud.com>
2023-03-01 15:12:05 +0900

    update: 必要なウィジェットを一通り追加

--- lib/presentation/detail_page.dart
+++ lib/presentation/detail_page.dart
@@ -7,9 +7,45 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(),
-          Row(),
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(Icons.favorite_border),
+                    Icon(Icons.favorite_border),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text("Venice"),
+                    Row(),
+                    Row(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [Icon(Icons.wifi), Text("Wifi")],
+              ),
+              Column(
+                children: [Icon(Icons.wifi), Text("Wifi")],
+              ),
+              Column(
+                children: [Icon(Icons.wifi), Text("Wifi")],
+              ),
+              Column(
+                children: [Icon(Icons.wifi), Text("Wifi")],
+              ),
+            ],
+          ),
           Text("詳細"),
           Card(),
           ElevatedButton(onPressed: null, child: Text("ホテルを予約する")),
